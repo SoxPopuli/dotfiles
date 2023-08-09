c9b3888eee5c81472eafaa9d5aa256a7141a35d4
Charlotte Smith <csmith@totallymoney.com>
2023-08-09 09:48:19 +0100

    Paralleletized update script

--- update.sh
+++ update.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-for dir in $(find . -maxdepth 1 -type d -not -path '.' -not -path './.git')
-do
-    cd "${dir}"
-    echo "Pulling ${dir}..."
+
+if [ $# -eq 0 ] 
+then
+    find . -maxdepth 1 -type d -not -path '.' -not -path './.git' | parallel $0
+else
+    cd "$1"
+    echo "Pulling $1..."
     git pull
     cd ..
-done
+fi
+
+
+#for dir in $(find . -maxdepth 1 -type d -not -path '.' -not -path './.git')
+#do
+#    cd "${dir}"
+#    echo "Pulling ${dir}..."
+#    git pull
+#    cd ..
+#done
