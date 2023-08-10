5e2ed1c79ba409e04500c8a81a90f5b03d7c8af5
Charlotte Smith <csmith@totallymoney.com>
2023-08-10 11:29:35 +0100

    Fixed link script

--- link.sh
+++ link.sh
@@ -4,5 +4,10 @@ if [ $# -eq 0 ]
 then
     find . -maxdepth 1 -type d -not -path '.' -not -path './.git' | parallel $0
 else
-    ln -shv "${1}" "${HOME}/.config/${1}"
+    # get full path
+    src=$(readlink -f ${1})
+    
+    # remove ./ from path
+    dst=$(echo "${HOME}/.config/${1}" | awk '{sub("/./", "/")}1')
+    ln -shv "${src}" "${dst}"
 fi
