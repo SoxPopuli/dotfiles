3313efff5caf0c6fa50907e2ba9af44d8df6112b
Charlotte Smith <charliesmith5019@gmail.com>
2024-02-13 21:30:14 +0000

    feat: added dot-repeat to aerial

--- nvim/after/plugin/aerial.lua
+++ nvim/after/plugin/aerial.lua
@@ -1,12 +1,24 @@
 local aerial = require('aerial')
+
+_G.aerial = {
+  next = function()
+    aerial.next(1)
+  end,
+  prev = function()
+    aerial.prev(1)
+  end,
+}
+
 aerial.setup({
   on_attach = function(bufnr)
     vim.keymap.set('n', ']a', function()
-      aerial.next(1)
-    end, { buffer = bufnr, nowait = true })
+      vim.go.operatorfunc = 'v:lua.aerial.next'
+      return 'g@l'
+    end, { buffer = bufnr, nowait = true, expr = true, desc = 'aerial: next' })
     vim.keymap.set('n', '[a', function()
-      aerial.prev(1)
-    end, { buffer = bufnr, nowait = true })
+      vim.go.operatorfunc = 'v:lua.aerial.prev'
+      return 'g@l'
+    end, { buffer = bufnr, nowait = true, expr = true, desc = 'aerial: prev' })
 
     vim.keymap.set('n', '<M-n>', function()
       aerial.next(1)
