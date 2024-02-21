ccaeed1e35406423b9cb89a20fe6ebfd9a8081d4
Charlotte Smith <charliesmith5019@gmail.com>
2024-02-21 19:26:17 +0000

    feat(fish): set git prompt to informative

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,8 @@ fish_vi_key_bindings
 set -gx EDITOR nvim
 set -gx fish_prompt_pwd_dir_length 0
 
+set -gx __fish_git_prompt_showupstream informative
+
 set -l fish_config_dir "$HOME/.config/fish"
 
 if test -f "$fish_config_dir/private.fish"
