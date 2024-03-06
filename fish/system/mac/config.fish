7265bab793bed979e2534031780a13dc2d59bec5
Charlotte Smith <charliesmith5019@gmail.com>
2024-03-06 13:29:34 +0000

    feat(fish): add mac settings

---
+++ fish/system/mac/config.fish
@@ -0,0 +1,9 @@
+fish_add_path "$HOME/.dotnet/tools"
+fish_add_path "/usr/local/opt/python/libexec/bin"
+fish_add_path "$HOME/bin"
+fish_add_path "/usr/local/bin"
+fish_add_path "$HOME/.cargo/bin"
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/Tools"
+
+set -gx GPG_TTY (tty)
