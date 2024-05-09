d907fa84eb0810cdc03e5201d64618d6a7f71d87
Charlotte Smith <charliesmith5019@gmail.com>
2024-05-09 11:47:38 +0100

    feat(fish): add direnv

--- fish/system/mac/config.fish
+++ fish/system/mac/config.fish
@@ -1,3 +1,12 @@
+# load fenv
+set fish_function_path $fish_function_path $HOME/Documents/plugin-foreign-env/functions
+
+# load nix-darwin
+fenv source /etc/static/bashrc 
+
+# load direnv
+direnv hook fish | source
+
 fish_add_path "$HOME/.dotnet/tools"
 fish_add_path "/usr/local/opt/python/libexec/bin"
 fish_add_path "$HOME/bin"
@@ -6,12 +15,4 @@ fish_add_path "$HOME/.cargo/bin"
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/Tools"
 
-fish_add_path /usr/local/opt/node@20/bin
-
 set -gx GPG_TTY (tty)
-
-# load fenv
-set fish_function_path $fish_function_path $HOME/Documents/plugin-foreign-env/functions
-
-# load nix-darwin
-fenv source /etc/static/bashrc 
