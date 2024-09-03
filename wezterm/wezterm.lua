415fcae0b37a0e77ae3ce5af0eedf9207e5fe757
Charlotte Smith <charliesmith5019@gmail.com>
2024-09-03 13:19:39 +0100

    fix(wezterm): fix renderer

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,13 +5,15 @@ local utils = require('utils')
 ---@class WeztermConfig
 local config = {
   font = wezterm.font_with_fallback({
-    { family = 'JetBrains Mono', weight = 'Bold' },
+    { family = 'JetBrains Mono' },
     'Symbols Nerd Font',
   }),
   font_size = 12.0,
   harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' },
   color_scheme = 'catppuccin-mocha',
   enable_tab_bar = false,
+
+  front_end = 'WebGpu',
 
   audible_bell = 'Disabled',
 
