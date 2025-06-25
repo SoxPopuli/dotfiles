e18cf99d425ce55c07c54116bacc30e499977a2d
Charlotte Smith <charliesmith5019@gmail.com>
2025-06-25 12:02:41 +0100

    feat(wezterm): map alt+\ to tilde

--- wezterm/keys.lua
+++ wezterm/keys.lua
@@ -5,51 +5,56 @@ local utils = require('utils')
 local act = require('wezterm').action
 
 local function mac_binds()
-  return {
-    -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
-    {
-      key = 'LeftArrow',
-      mods = 'OPT',
-      action = act.SendKey({
-        key = 'b',
-        mods = 'ALT',
-      }),
-    },
-    {
-      key = 'RightArrow',
-      mods = 'OPT',
-      action = act.SendKey({ key = 'f', mods = 'ALT' }),
-    },
-    {
-      key = 'LeftArrow',
-      mods = 'CMD',
-      --action = act.SendKey({ key = 'a', mods = 'CTRL' }),
-      action = act.SendString('\x1b[H'),
-    },
-    {
-      key = 'RightArrow',
-      mods = 'CMD',
-      action = act.SendKey({ key = 'e', mods = 'CTRL' }),
-    },
-  }
+    return {
+        -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+        {
+            key = 'LeftArrow',
+            mods = 'OPT',
+            action = act.SendKey({
+                key = 'b',
+                mods = 'ALT',
+            }),
+        },
+        {
+            key = 'RightArrow',
+            mods = 'OPT',
+            action = act.SendKey({ key = 'f', mods = 'ALT' }),
+        },
+        {
+            key = 'LeftArrow',
+            mods = 'CMD',
+            --action = act.SendKey({ key = 'a', mods = 'CTRL' }),
+            action = act.SendString('\x1b[H'),
+        },
+        {
+            key = 'RightArrow',
+            mods = 'CMD',
+            action = act.SendKey({ key = 'e', mods = 'CTRL' }),
+        },
+    }
 end
 
 function M.maps()
-  local success, system = pcall(utils.capture, 'uname -s')
+    local success, system = pcall(utils.capture, 'uname -s')
 
-  local maps = {
-    {
-      key = ' ',
-      mods = 'SHIFT',
-      action = act.SendKey({ key = '_' }),
-    },
-  }
+    local maps = {
+        {
+            key = ' ',
+            mods = 'SHIFT',
+            action = act.SendKey({ key = '_' }),
+        },
+        {
+            key = '\\',
+            mods = 'ALT',
+            action = act.SendKey({ key = '~' }),
+        },
+    }
 
-  if success and system == 'Darwin' then
-    utils.merge(maps, mac_binds())
-  end
+    if success and system == 'Darwin' then
+        utils.merge(maps, mac_binds())
+    end
 
-  return maps
+    return maps
 end
 
 return M
